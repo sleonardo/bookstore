f63e0a06a769d4e5f670b70935411503f559e8b4
Leonardo Simoza <simozaleonardo@gmail.com>
2021-07-17 19:43:04 -0500

    Initial Commit

---
+++ Bookstore/BookstoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookstoreApp.swift
+//  Bookstore
+//
+//  Created by Leonardo Simoza on 17/07/21.
+//
+
+import SwiftUI
+
+@main
+struct BookstoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
